a704338ba797959dcfff40f82bd47eb3f8edcbcd
Ivan Utsalo <utsaloosi@gmail.com>
2015-10-03 22:15:07 -0700

    Login flow (user can login and sign up).

--- NGChat/app/src/main/java/com/sidelance/ngchat/LoginActivity.java
+++ NGChat/app/src/main/java/com/sidelance/ngchat/LoginActivity.java
@@ -1,21 +1,37 @@
 package com.sidelance.ngchat;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     protected TextView mSignUpTextView;
 
+    protected EditText mUsername;
+    protected EditText mPassword;
+    protected Button mLoginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -37,6 +53,62 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(signUpIntent);
             }
         });
+
+        mUsername = (EditText) findViewById(R.id.userNameEditText);
+        mPassword = (EditText) findViewById(R.id.passwordEditText);
+        mLoginButton = (Button) findViewById(R.id.loginButton);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+
+                username = username.trim();
+                password = password.trim();
+
+                if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                    builder.setMessage(R.string.SIGN_UP_ERROR_MESSAGE)
+                            .setTitle(R.string.SIGN_UP_ERROR_TITLE)
+                            .setPositiveButton(android.R.string.ok, null);
+
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
+                } else {
+
+                    setProgressBarIndeterminateVisibility(true);
+                    ParseUser.logInInBackground(username, password, new LogInCallback() {
+                        @Override
+                        public void done(ParseUser user, ParseException e) {
+                            setProgressBarIndeterminateVisibility(false);
+
+                            if (e == null){
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+
+                            } else{
+
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setMessage(e.getMessage())
+                                        .setTitle(R.string.LOGIN_ERROR_TITLE)
+                                        .setPositiveButton(android.R.string.ok, null);
+
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
+
+
     }
 
 }
